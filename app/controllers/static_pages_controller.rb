0e078bf558c7f7293691d1a4fe8a227af9b1b61c
Radhikaa <radhikab@thoughtworks.com>
2014-12-05 13:49:45 +0530

    [Radhikaa] Initial Run

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,9 @@
-class ApplicationController < ActionController::Base
+class StaticPagesController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def application
+  def index
     @welcomeMsg = "Hello World"
   end
 end
